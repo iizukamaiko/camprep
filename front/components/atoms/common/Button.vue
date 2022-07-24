f764452cfd6c8375d3937a81010af02c089b26f0
iizukamaiko <iizkmaiko@gmail.com>
2022-07-24 16:18:08 +0900

    add Button component

---
+++ front/components/atoms/common/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-btn
+		depressed
+		color=#E4CC49
+		:to="url">
+		{{ text }}
+	</v-btn>
+</template>
+
+<style lang="scss" scoped>
+v-btn {
+  background-color: $yellow;
+	text-align: center;
+}
+</style>
+
+<script lang="ts">
+export default ({
+	props: {
+		text: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String
+    }
+	}
+})
+</script>
